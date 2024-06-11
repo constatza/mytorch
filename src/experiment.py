03d17cd66a06b42ca42fcd2b8705a97a94b4d479
constatza <29166969+constatza@users.noreply.github.com>
2024-06-11 18:23:13 +0300

    refactor and decompose classes

--- src/experiment.py
+++ src/experiment.py
@@ -1,45 +1,33 @@
 import importlib
-import inspect
 import os
+from typing import Optional, Union, Dict
 from collections.abc import Iterable
 from itertools import product
 
 import numpy as np
-import tomli_w as tw
 import torch
-from torch.utils.data import DataLoader
+from utils import smart_load_tensors, get_proper_convolution_shape
 
-from parsers import TOMLParser
-
-
-def filtered(func):
-    def wrapper(*args, **kwargs):
-        params = inspect.signature(func).parameters
-        filtered = {k: v for k, v in kwargs.items() if k in params}
-        return func(*args, **filtered)
-
-    return wrapper
+from datasets import create_dataloaders
+from utils import filtered
 
 
 @filtered
 class Experiment:
-    def __init__(self, model=None, uid=None, x_train=None, x_test=None, y_train=None, y_test=None,
+    def __init__(self, model=None, uid=None, dataloader_train=None, dataloader_val=None,
                  optimizer=None, criterion=None, lr=None, batch_size=None, num_epochs=None, checkpoint_dir=None,
                  epoch_print_interval=10):
         self.name = model.__class__.__name__ + '_' + str(uid)
         self.model = model
+        self.dataloader_train = dataloader_train
+        self.dataloader_val = dataloader_val
         self.id = uid
-        self.x_train = x_train
-        self.x_test = x_test
-        self.y_train = y_train
-        self.y_test = y_test
         self.optimizer = optimizer(self.model.parameters(), lr=lr)
         self.criterion = criterion
         self.lr = lr
         self.batch_size = batch_size
         self.num_epochs = num_epochs
         self.checkpoint_relative_tol = 0.5
-        self.dataloader_train, self.dataloader_val = create_dataloaders(x_train, y_train, x_test, y_test, batch_size)
         self.checkpoint_path = os.path.join(checkpoint_dir, self.name + '.pt')
         os.makedirs(checkpoint_dir, exist_ok=True)
         self.epoch_print_interval = epoch_print_interval
@@ -48,6 +36,7 @@ class Experiment:
 
     def run(self, *args, **kwargs):
         return self.train(*args, **kwargs)
+
 
     def train(self) -> tuple:
         """Training loop for the model with both training and validation loss."""
@@ -65,13 +54,8 @@ class Experiment:
             # Training
             self.model.train()
             train_loss = 0
-            for batch in self.dataloader_train:
+            for x_batch, y_batch in self.dataloader_train:
 
-                if len(batch) == 2:
-                    x_batch, y_batch = batch
-                else:
-                    x_batch = batch[0]
-                    y_batch = x_batch
                 x_batch = x_batch.to(device)
                 y_batch = y_batch.to(device)
 
@@ -80,28 +64,22 @@ class Experiment:
                 loss = criterion(y_pred, y_batch)
                 loss.backward()
                 optimizer.step()
-                train_loss += loss.item()
+                train_loss += loss.item() * x_batch.size(0)
             train_losses.append(train_loss / len(self.dataloader_train))
 
             # Validation
             val_loss = 0
             self.model.eval()
             with torch.no_grad():
-                for batch in self.dataloader_val:
-                    if len(batch) == 2:
-                        x_batch, y_batch = batch
-                    else:
-                        x_batch = batch[0]
-                        y_batch = x_batch
+                for x_batch, y_batch in self.dataloader_val:
                     x_batch = x_batch.to(device)
                     y_batch = y_batch.to(device)
                     loss_val = criterion(self.model(x_batch), y_batch)
-                    val_loss += loss_val.item()
+                    val_loss += loss_val.item() * x_batch.size(0)
                 val_losses.append(val_loss / len(self.dataloader_val))
-            # print with scientific notation and 6 decimal places
-            if (epoch + 1) % self.epoch_print_interval == 0:
-                print(
-                    f'Epoch {epoch + 1}/{self.num_epochs} | Train Loss: {train_losses[-1]:.6e} | Val Loss: {val_losses[-1]:.6e}')
+
+            self.print_losses(train_losses[-1], val_losses[-1], epoch)
+
 
             # Check if the validation loss has decreased by more than the tolerance
             if relative_tolerance(min_val_loss, val_losses) > self.checkpoint_relative_tol:
@@ -115,6 +93,15 @@ class Experiment:
         return train_losses, val_losses
 
 
+    def print_losses(self, train_loss, val_loss, epoch) -> None:
+        epoch_for_print = epoch + 1
+        if epoch_for_print % self.epoch_print_interval == 0:
+            text = f'Epoch {epoch_for_print}/{self.num_epochs} | Train Loss: {train_loss:.5e} | Val Loss: {val_loss:.5e}'
+            print(text)
+
+
+
+
 def relative_tolerance(min_loss, losses):
     if len(losses) > 5:
         mean = np.mean(losses[-5:])
@@ -123,22 +110,6 @@ def relative_tolerance(min_loss, losses):
     return (min_loss - mean) / mean
 
 
-def create_dataloaders(x_train=None, y_train=None, x_test=None, y_test=None, batch_size=32):
-    """Create dataloaders from the data."""
-    x_train = torch.tensor(x_train, dtype=torch.float32)
-    x_test = torch.tensor(x_test, dtype=torch.float32)
-    if y_train is not None:
-        y_train = torch.tensor(y_train, dtype=torch.float32)
-        y_test = torch.tensor(y_test, dtype=torch.float32)
-        train_dataset = torch.utils.data.TensorDataset(x_train, y_train)
-        val_dataset = torch.utils.data.TensorDataset(x_test, y_test)
-    else:
-        train_dataset = torch.utils.data.TensorDataset(x_train)
-        val_dataset = torch.utils.data.TensorDataset(x_test)
-
-    dataloader_train = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    dataloader_val = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-    return dataloader_train, dataloader_val
 
 
 class Parameters:
@@ -161,78 +132,107 @@ class Parameters:
         return new_keys
 
 
-class Analysis:
-    """Creates all possible combinations of parameters for the experiment using cartesian product."""
 
-    def __init__(self, parser: TOMLParser, optimizer, criterion, new=False) -> None:
 
-        self.parser = parser
+class AnalysisLoader:
+
+    def __init__(self, paths_dict: Dict, delete_old: bool = False, convolution_dims: int = 0) -> None:
+        self.paths_dict = paths_dict
+        self.convolution_dims = convolution_dims
         self.load_networks()
         self.load_data()
-        self.optimizer = optimizer
-        self.criterion = criterion
-
-        joined = {**self.model_parameters, **self.models_dict}
+        joined = {**self.model_variable_parameters, **self.models_dict}
         self.model_combinations = Parameters.create_combinations(joined)
         self.training_combinations = Parameters.create_combinations(self.training_parameters)
         self.num_experiments = len(self.model_combinations) * len(self.training_combinations)
         self.experiments_ran = []
-        if new:
+        if delete_old:
             self.delete_old_files()
         else:
             self.decide_which_to_run()
 
     def delete_old_files(self):
-        dirpath = self.parser['paths']['output']['root']
+        dirpath = self.paths_dict['paths']['output']['root']
         if os.path.basename(dirpath).lower() not in ('output', 'out', 'results'):
             for root, dirs, files in os.walk(dirpath):
                 for file in files:
                     os.remove(os.path.join(root, file))
 
     def decide_which_to_run(self):
-        parameters_dir = self.parser['paths']['output']['parameters']
+        parameters_dir = self.paths_dict['paths']['output']['parameters']
         if os.path.exists(parameters_dir):
             for file_name in os.listdir(parameters_dir):
                 # get id from filename end
                 experiment_id = file_name.split('.')[0].split('_')[-1]
                 self.experiments_ran.append(int(experiment_id))
 
+    def load_data(self) -> None:
+        self.training_parameters = self.paths_dict['training']
+        x_train = self.paths_dict['paths']['input']['x-train']
+        x_test = self.paths_dict['paths']['input']['x-test']
+        y_train = self.paths_dict['paths']['input']['y-train']
+        y_test = self.paths_dict['paths']['input']['y-test']
+
+        self.x_train = smart_load_tensors(x_train, self.convolution_dims)
+        self.x_test = smart_load_tensors(x_test, self.convolution_dims)
+
+        if x_train != y_train and x_test != y_test:
+            self.y_train = smart_load_tensors(y_train, self.convolution_dims)
+            self.y_test = smart_load_tensors(y_test, self.convolution_dims)
+        else:
+            self.y_train = None
+            self.y_test = None
+
+        self.dataloader_train, self.dataloader_val = create_dataloaders(self.x_train, self.x_test, self.y_train,
+                                                                        self.y_test)
+        self.is_autoencoder = self.dataloader_val.dataset.is_autoencoder
+        self.x_shape = self.dataloader_val.dataset.x_shape
+        self.y_shape = self.dataloader_val.dataset.y_shape
+        self.input_shape = get_proper_convolution_shape(self.x_shape, self.convolution_dims)
+        self.output_shape = get_proper_convolution_shape(self.y_shape, self.convolution_dims)
+
+
+
+        self.model_variable_parameters = self.paths_dict['model']
+        self.model_shape_parameters = {'input_size': self.input_shape[-1],
+                                        'output_size': self.output_shape[-1],
+                                        'input_shape': self.input_shape,
+                                        'output_shape': self.output_shape}
+
+        print(f'Loaded data with input shape {self.input_shape} and output shape {self.output_shape}.')
+
     def load_networks(self):
-        parser = self.parser
-        module_paths = [string.split('.') for string in parser['model']['networks']]
+        paths_dict = self.paths_dict
+        module_paths = [string.split('.') for string in paths_dict['model']['networks']]
         modules = ['networks.' + ''.join(module_paths[:-1]) for module_paths in module_paths]
         classes = [module_paths[-1] for module_paths in module_paths]
 
         modules = [importlib.import_module(module) for module in modules]
         models = [getattr(module, model) for module, model in zip(modules, classes)]
-        self.parser['model']['networks'] = models
+        self.paths_dict['model']['networks'] = models
         self.models = [filtered(model) for model in models]
         self.models_dict = {'model': [model for model in self.models]}
 
-    def load_data(self):
-        self.model_parameters = self.parser['model']
-        self.training_parameters = self.parser['training']
-        x_train = self.parser['paths']['input']['x-train']
-        x_test = self.parser['paths']['input']['x-test']
-        if x_train.endswith('.npy'):
-            self.x_train = np.load(x_train)
-        else:
-            self.x_train = torch.load(x_train)
-        if x_test.endswith('.npy'):
-            self.x_test = np.load(x_test)
-        else:
-            self.x_test = torch.load(x_test)
-        self.input_shape = self.x_train.shape[2:]
-        try:
-            self.y_train = np.load(self.parser['paths']['input']['y-train'])
-            self.y_test = np.load(self.parser['paths']['input']['y-test'])
 
-        except KeyError:
-            self.y_train = None
-            self.y_test = None
+class Analysis:
+    """Creates all possible combinations of parameters for the experiment using cartesian product."""
 
-    def run(self):
-        writer = Writer(self.parser)
+    def __init__(self, analysis_loader: AnalysisLoader, logger, optimizer, criterion, is_convolutional=False) -> None:
+
+        self.loader = analysis_loader
+        self.is_convolutional = is_convolutional
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.model_combinations = analysis_loader.model_combinations
+        self.training_combinations = analysis_loader.training_combinations
+        self.num_experiments = analysis_loader.num_experiments
+        self.experiments_ran = analysis_loader.experiments_ran
+        self.model_shape_parameters = analysis_loader.model_shape_parameters
+        self.dataloader_train = analysis_loader.dataloader_train
+        self.dataloader_val = analysis_loader.dataloader_val
+        self.logger = logger
+    def run(self) -> None:
+        logger = self.logger
         losses = []
         for model_id, model_parameters in self.model_combinations.items():
             for train_id, training_parameters in self.training_combinations.items():
@@ -242,70 +242,24 @@ class Analysis:
                     # separate stdout line
                     print('-' * 80)
                     print(f'Running experiment {experiment_id}/{self.num_experiments}')
-                    model = model_parameters['model'](self.input_shape, **model_parameters)
-                    experiment = Experiment(model, x_train=self.x_train, x_test=self.x_test,
+                    model = model_parameters['model'](**self.model_shape_parameters, **model_parameters)
+                    experiment = Experiment(model, dataloader_train=self.dataloader_train, dataloader_val=self.dataloader_val,
                                             optimizer=self.optimizer, criterion=self.criterion, uid=experiment_id,
                                             **training_parameters,
-                                            checkpoint_dir=self.parser['paths']['output']['models'])
+                                            checkpoint_dir=self.logger.paths_dict['output']['models'])
                     # search parameters if experiment has already been run
 
                     try:
                         train_loss, val_loss = experiment.train()
                         losses.append(val_loss[-1])
-                        writer.log(f'Experiment {experiment_id:d} completed successfully.')
-                        writer.write(f'{experiment.name}.pt', model, dirname='models')
+                        logger.log(f'Experiment {experiment_id:d} completed successfully.')
+                        logger.write(f'{experiment.name}.pt', model, dirname='models')
                         parameters = {**model_parameters, **training_parameters, 'id': experiment_id}
-                        writer.write(f'{experiment.name}.toml', parameters, dirname='parameters')
-                        writer.write(f'losses', val_loss[-1])
+                        logger.write(f'{experiment.name}.toml', parameters, dirname='parameters')
+                        logger.write(f'losses', val_loss[-1])
                     except Exception as e:
                         raise e
-                        writer.error(f'Experiment {experiment_id} failed with error: {e}')
+                        logger.error(f'Experiment {experiment_id} failed with error: {e}')
 
 
-class Writer:
-    def __init__(self, parser: TOMLParser):
-        self.parser = parser
 
-    def write(self, name, data, dirname='root'):
-        suffix = name.split('.')[-1]
-
-        # select the correct extension and method
-        if suffix == 'npy':
-            saver = np.save
-            mode = 'wb'
-        elif suffix in ('pt', 'pth'):
-            saver = torch.save
-            mode = 'wb'
-        elif suffix == 'csv':
-            saver = np.savetxt
-            mode = 'a'
-        elif suffix == 'toml':
-            def saver(dictionary, file):
-                for key, value in dictionary.items():
-                    # check if value is a class
-                    if inspect.isclass(value) or inspect.isfunction(value):
-                        dictionary[key] = None
-                dictionary = {k: v for k, v in dictionary.items() if v is not None}
-                tw.dump(dictionary, file)
-
-            mode = 'wb'
-        else:
-            def saver(data, file):
-                if not isinstance(data, str):
-                    data = str(data)
-                file.write(data)
-                file.write('\n')
-
-            mode = 'a'
-
-        directory = self.parser['paths']['output'][dirname]
-        os.makedirs(directory, exist_ok=True)
-        path = os.path.join(directory, name)
-        with open(path, mode) as file:
-            saver(data, file)
-
-    def log(self, message):
-        return self.write('log', message)
-
-    def error(self, message):
-        return self.write('error', message)
