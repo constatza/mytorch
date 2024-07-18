dfa24419736c2974a5c33790c8aaba17abc55365
constatza <29166969+constatza@users.noreply.github.com>
2024-07-18 20:06:55 +0300

    integrate pytorch lightning

--- src/mytorch/networks/ffnns/mlps.py
+++ src/mytorch/networks/ffnns/mlps.py
@@ -1,37 +1,94 @@
 import torch.nn as nn
+from lightning import LightningModule
+from torch.optim import RAdam
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+from mytorch.mytypes import ListLike
 
 
-class ConstantHiddenSizeFFNN(nn.Module):
+class FeedForwardNN(LightningModule):
+
     def __init__(
         self,
-        input_size=None,
-        output_size=None,
-        hidden_layers=None,
-        hidden_size=None,
-        activation=nn.Sigmoid,
+        layers: ListLike = None,
+        activation: nn.functional = nn.functional.relu,
+        lr: float = 1e-3,
     ):
-        super(ConstantHiddenSizeFFNN, self).__init__()
-
-        self.input_size = input_size
-        self.output_size = output_size
-        self.hidden_layers = hidden_layers
-        self.hidden_size = hidden_size
+        super(FeedForwardNN, self).__init__()
+        self.save_hyperparameters()
+        self.linear_layers = nn.ModuleList(
+            nn.Linear(layers[i], layers[i + 1]) for i in range(len(layers) - 1)
+        )
         self.activation = activation
+        self.norms = nn.ModuleList(
+            nn.BatchNorm1d(layers[i]) for i in range(len(layers) - 1)
+        )
 
-        self.layers = nn.ModuleList()
+    def forward(self, x):
+        for i, layer in enumerate(self.linear_layers):
+            x = self.norms[i](x)
+            x = layer(x)
+            x = self.activation(x)
+            x = nn.functional.dropout(x, p=0.5)
+        return x
 
-        self.layers.append(nn.Linear(input_size, hidden_size))
-        self.layers.append(activation())
+    def configure_optimizers(self):
+        optimizer = RAdam(self.parameters(), lr=self.hparams.lr)
+        scheduler = ReduceLROnPlateau(
+            optimizer, mode="min", factor=0.5, patience=10, verbose=True
+        )
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "monitor": "val_loss",
+            },
+        }
 
-        for _ in range(hidden_layers - 1):
-            self.layers.append(nn.Linear(hidden_size, hidden_size))
-            self.layers.append(activation())
-            # self.layers.append(nn.Dropout(0.5))
-            # self.layers.append(nn.BatchNorm1d(hidden_size))
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.forward(x)
+        loss = self.loss(y_hat, y)
+        self.log("train_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+        return loss
 
-        self.layers.append(nn.Linear(hidden_size, output_size))
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.loss(y_hat, y)
+        self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.loss(y_hat, y)
+        self.log("test_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+        return loss
+
+    @staticmethod
+    def loss(y_hat, y):
+        return nn.functional.mse_loss(y_hat, y)
+
+    def on_validation_epoch_end(self) -> None:
+        lr = self.trainer.optimizers[0].param_groups[0]["lr"]
+        self.log("lr", lr, on_step=False, on_epoch=True, prog_bar=True)
+
+
+class ConstantHiddenSizeFFNN(FeedForwardNN):
+    def __init__(
+        self,
+        input_size: int = None,
+        output_size: int = None,
+        hidden_size: int = None,
+        num_layers: int = None,
+        **kwargs,
+    ):
+        layers = [input_size] + [hidden_size] * num_layers + [output_size]
+        super(ConstantHiddenSizeFFNN, self).__init__(layers, **kwargs)
 
     def forward(self, x):
         for layer in self.layers:
             x = layer(x)
+            x = self.activation(x)
         return x
