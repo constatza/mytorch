27085184f5dc435368b1b8e718d4eab6f4412a60
constatza <29166969+constatza@users.noreply.github.com>
2024-07-31 21:49:07 +0300

    minor updates

--- src/mytorch/mytypes.py
+++ src/mytorch/mytypes.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import TypeVar, List, Tuple, Annotated, Callable, Dict
+from typing import TypeVar, List, Tuple, Annotated, Callable, Dict, Iterable
 
 from numpy import ndarray
 from pydantic import FilePath, DirectoryPath, BeforeValidator, validate_call
@@ -10,10 +10,14 @@ type MaybeListLike[T] = List[T] | Tuple[T] | T
 
 
 @validate_call
-def ensure_iterable(x: MaybeListLike[T]) -> Tuple[T]:
-    if not isinstance(x, (list, tuple)):
+def ensure_tuple(x: MaybeListLike[T], remove_nones=True) -> Tuple[T]:
+    if not isinstance(x, Iterable):
         x = (x,)
-    return tuple(x)
+    else:
+        x = tuple(x)
+    if remove_nones:
+        x = tuple(i for i in x if i is not None)
+    return x
 
 
 @validate_call
@@ -22,7 +26,7 @@ def ensure_dir_exists(p: Path):
     return p
 
 
-type ListLike[T] = Annotated[Tuple[T] | List[T], BeforeValidator(ensure_iterable)]
+type TupleLike[T] = Annotated[Tuple[T] | List[T], BeforeValidator(ensure_tuple)]
 type NoInit[T] = Callable[..., T]
 type CreateIfNotExistsDir = Annotated[DirectoryPath, BeforeValidator(ensure_dir_exists)]
 type PathLike = FilePath | DirectoryPath | Path
